5cd13f78ce606382f15b2d30a94fdcbca94a80e2
cale-i <calei078029@gmail.com>
2020-10-19 04:34:38 +0900

    remove double quotation

--- security_group_module/security_group/main.tf
+++ security_group_module/security_group/main.tf
@@ -28,6 +28,6 @@ resource "aws_security_group_rule" "egress" {
   security_group_id = aws_security_group.default.id
 }
 
-output "security_goup_id" {
-  value = "aws_security_group.default.id"
+output "security_group_id" {
+  value = aws_security_group.default.id
 }
