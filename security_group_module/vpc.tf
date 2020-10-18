281ba623ca1b4710cc78eae3bd8da471fb594674
cale-i <calei078029@gmail.com>
2020-10-19 00:34:33 +0900

    security_group_module

---
+++ security_group_module/vpc.tf
@@ -0,0 +1,13 @@
+variable "prefix" {
+  type    = string
+  default = "Network"
+}
+
+resource "aws_vpc" "example" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.prefix}"
+  }
+}
